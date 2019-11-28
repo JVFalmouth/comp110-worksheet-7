c1cb694e2dd5e434318d8629635b5173c186b297
James Vanstone <jv228882@falmouth.ac.uk>
2019-11-28 17:19:26 +0000

    All tests passed successfully!

--- comp110-worksheet-7/DirectoryUtils.cs
+++ comp110-worksheet-7/DirectoryUtils.cs
@@ -21,40 +21,190 @@ namespace comp110_worksheet_7
 			return File.GetAttributes(path).HasFlag(FileAttributes.Directory);
 		}
 
-		// Return the total size, in bytes, of all the files below the given directory
-		public static long GetTotalSize(string directory)
-		{
-			throw new NotImplementedException();
-		}
+        // Return the total size, in bytes, of all the files below the given directory
+        public static long GetTotalSize(string directory)
+        {
+            long size = 0;
+            Stack<string> searchStack = new Stack<string>();
+            searchStack.Push(directory);
+            while (searchStack.Count != 0)
+            {
+                var n = searchStack.Pop();
+                Console.WriteLine(n);
+                if (IsDirectory(n))
+                {
+                    var directories = Directory.GetDirectories(n);
+                    var files = Directory.GetFiles(n);
+                    foreach (string dir in directories)
+                    {
+                        searchStack.Push(dir);
+                    }
+                    foreach (string file in files)
+                    {
+                        size += GetFileSize(file);
+                    }
+                }
+                else
+                {
+                    size += GetFileSize(n);
+                }
+            }
+            return size;
+        }
 
 		// Return the number of files (not counting directories) below the given directory
 		public static int CountFiles(string directory)
 		{
-			throw new NotImplementedException();
-		}
+            int count = 0;
+            Stack<string> searchStack = new Stack<string>();
+            searchStack.Push(directory);
+            while (searchStack.Count != 0)
+            {
+                var n = searchStack.Pop();
+                Console.WriteLine(n);
+                if (IsDirectory(n))
+                {
+                    var directories = Directory.GetDirectories(n);
+                    var files = Directory.GetFiles(n);
+                    foreach (string dir in directories)
+                    {
+                        searchStack.Push(dir);
+                    }
+                    foreach (string file in files)
+                    {
+                        count ++;
+                    }
+                }
+                else
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
 
 		// Return the nesting depth of the given directory. A directory containing only files (no subdirectories) has a depth of 0.
 		public static int GetDepth(string directory)
 		{
-			throw new NotImplementedException();
+            int depth = 0;
+            if (Directory.GetDirectories(directory).Length == 0)
+            {
+                return depth;
+            }
+            else
+            {
+                depth++;
+                var subDirectories = Directory.GetDirectories(directory);
+                foreach (string dir in subDirectories)
+                {
+                    depth += GetDepth(dir);
+                }
+                return depth;
+            }     
 		}
 
 		// Get the path and size (in bytes) of the smallest file below the given directory
 		public static Tuple<string, long> GetSmallestFile(string directory)
 		{
-			throw new NotImplementedException();
+            Tuple<string, long> smallestFile = new Tuple<string, long>("", long.MaxValue);
+            Stack<string> searchStack = new Stack<string>();
+            searchStack.Push(directory);
+            while (searchStack.Count != 0)
+            {
+                var n = searchStack.Pop();
+                Console.WriteLine(n);
+                if (IsDirectory(n))
+                {
+                    var directories = Directory.GetDirectories(n);
+                    var files = Directory.GetFiles(n);
+                    foreach (string dir in directories)
+                    {
+                        searchStack.Push(dir);
+                    }
+                    foreach (string file in files)
+                    {
+                        searchStack.Push(file);
+                    }
+                }
+                else
+                {
+                    if ((GetFileSize(n) < smallestFile.Item2))
+                    {
+                        smallestFile = new Tuple<string, long>(n, GetFileSize(n));
+                    }
+                }
+            }                
+            return smallestFile;
 		}
 
 		// Get the path and size (in bytes) of the largest file below the given directory
 		public static Tuple<string, long> GetLargestFile(string directory)
 		{
-			throw new NotImplementedException();
-		}
+            Tuple<string, long> largestFile = new Tuple<string, long>("", 0);
+            Stack<string> searchStack = new Stack<string>();
+            searchStack.Push(directory);
+            while (searchStack.Count != 0)
+            {
+                var n = searchStack.Pop();
+                Console.WriteLine(n);
+                if (IsDirectory(n))
+                {
+                    var directories = Directory.GetDirectories(n);
+                    var files = Directory.GetFiles(n);
+                    foreach (string dir in directories)
+                    {
+                        searchStack.Push(dir);
+                    }
+                    foreach (string file in files)
+                    {
+                        searchStack.Push(file);
+                    }
+                }
+                else
+                {
+                    if ((GetFileSize(n) > largestFile.Item2))
+                    {
+                        largestFile = new Tuple<string, long>(n, GetFileSize(n));
+                    }
+                }
+            }
+            return largestFile;
+        }
 
 		// Get all files whose size is equal to the given value (in bytes) below the given directory
 		public static IEnumerable<string> GetFilesOfSize(string directory, long size)
 		{
-			throw new NotImplementedException();
-		}
+            List<string> correctFiles = new List<string>();
+
+            Stack<string> searchStack = new Stack<string>();
+            searchStack.Push(directory);
+            while (searchStack.Count != 0)
+            {
+                var n = searchStack.Pop();
+                Console.WriteLine(n);
+                if (IsDirectory(n))
+                {
+                    var directories = Directory.GetDirectories(n);
+                    var files = Directory.GetFiles(n);
+                    foreach (string dir in directories)
+                    {
+                        searchStack.Push(dir);
+                    }
+                    foreach (string file in files)
+                    {
+                        searchStack.Push(file);
+                    }
+                }
+                else
+                {
+                    if ((GetFileSize(n) == size))
+                    {
+                        correctFiles.Add(n);
+                    }
+                }
+            }
+            return correctFiles;
+
+        }
 	}
 }
